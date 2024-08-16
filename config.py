88addd7eb85030c558c88958ed83169d3f87a598
Jaeung <jwyun0330@naver.com>
2024-08-16 11:58:58 +0900

    commit

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ def parser():
     arg_parser.add_argument("--test_dir", default="./test_data", type=str, help="path to test directory")
     arg_parser.add_argument("--epochs", default=100, type=int, help="number of epochs")
     arg_parser.add_argument("--batch_size", default=64, type=int, help="batch size")
-    arg_parser.add_argument("--learning_rate", default=1e-4, type=int, help="learning rate")
+    arg_parser.add_argument("--learning_rate", default=1e-4, type=float, help="learning rate")
     arg_parser.add_argument("--seed", default=42, type=int, help="random seed")
     arg_parser.add_argument("--device", default="cpu", type=str, help="use cpu or cuda")
     arg_parser.add_argument("--img_size", default=256, type=int, help="image size")
