d6a4be4df0e0d5e9a92a8f30dcb29aad2d740848
Jaeung <jwyun0330@naver.com>
2024-08-16 16:26:51 +0900

    commit

--- model/classifier.py
+++ model/classifier.py
@@ -9,27 +9,27 @@ class Classifier(nn.Module):
     def __init__(self, input_size, output_size):
         super().__init__()
         self.conv1 = nn.Sequential(
-            torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            torch.nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, padding=1),
             torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.2),
 
-            torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            torch.nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1),
             torch.nn.BatchNorm2d(64),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.2),
 
-            torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
             torch.nn.BatchNorm2d(128),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.2),
 
-            torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            torch.nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, padding=1),
             torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.2),
         )
-        self.fc = nn.Linear(in_features=256*input_size*input_size, out_features=output_size)
+        self.fc = nn.Linear(in_features=16*input_size*input_size, out_features=output_size)
 
     def forward(self, x):
         x = self.conv1(x)
